09f45aa4f1ddc7e249cfe16ca1dfc8c9c49f2850
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-23 19:11:47 +0100

    use controller-routes for stock making

--- src/modules/stock/stock.routes.ts
+++ src/modules/stock/stock.routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
-import { getStockOverview, getStockByDrugId } from "./stock.services.js";
+import { stockController } from "./stock.controller.js";
 import { requireAuth } from "../../plugins/auth.plugins.js";
 
 export async function stockRoutes(
@@ -9,38 +9,12 @@ export async function stockRoutes(
   // GET /api/stock - Vue stock agrégée
   fastify.get("/", {
     preHandler: [requireAuth],
-    handler: async (_request, reply) => {
-      const stock = await getStockOverview();
-      return reply.status(200).send({
-        success: true,
-        data: {
-          items: stock,
-          summary: {
-            totalDrugs: stock.length,
-            drugsInStock: stock.filter((s) => s.totalQuantity > 0).length,
-            drugsBelowMin: stock.filter((s) => s.isBelowMin).length,
-            drugsCritical: stock.filter((s) => s.isCritical).length,
-            totalValueCDF: stock.reduce(
-              (sum, s) =>
-                sum +
-                (s.unitPriceCDF
-                  ? s.unitPriceCDF.toNumber() * s.totalQuantity
-                  : 0),
-              0
-            ),
-          },
-        },
-      });
-    },
+    handler: stockController.getOverview,
   });
 
   // GET /api/stock/:drugId - Détail stock d'un médicament
   fastify.get("/:drugId", {
     preHandler: [requireAuth],
-    handler: async (request, reply) => {
-      const { drugId } = request.params as { drugId: string };
-      const detail = await getStockByDrugId(drugId);
-      return reply.status(200).send({ success: true, data: detail });
-    },
+    handler: stockController.getOne,
   });
 }
